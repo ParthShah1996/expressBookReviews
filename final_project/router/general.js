c42ff833c4bf694af5592bca20bd13743f3aad4d
ParthShah1996 <parth.rs96@gmail.com>
2026-07-14 06:49:35 -0400

    added async functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require("axios");
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -80,5 +80,60 @@ public_users.get('/review/:isbn',function (req, res) {
   res.send(books[isbn].reviews)
 //   return res.status(300).json({message: "Yet to be implemented"});
 });
+
+public_users.get("/async/books", async (req, res) => {
+    try {
+        const response = await axios.get("http://localhost:5000/");
+        return res.status(200).json(response.data);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+public_users.get("/async/isbn/:isbn", async (req, res) => {
+    const isbn = req.params.isbn;
+    try {
+        const response = await axios.get(
+            `http://localhost:5000/isbn/${isbn}`
+        );
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+
+    }
+});
+
+public_users.get("/async/author/:author", async (req, res) => {
+    const author = req.params.author;
+    try {
+        const response = await axios.get(
+            `http://localhost:5000/author/${author}`
+        );
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+public_users.get("/async/title/:title", async (req, res) => {
+    const title = req.params.title;
+    try {
+        const response = await axios.get(
+            `http://localhost:5000/title/${title}`
+        );
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
 
 module.exports.general = public_users;
