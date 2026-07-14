c5cf3ba8a91be1125665f31fcd362521506f7073
ParthShah1996n <parth.rs96@gmail.com>
2026-07-14 02:39:05 -0400

    get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,7 +27,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  const bookKeys = Object.keys(books);
+  const filteredBooks = [];
+  bookKeys.forEach(key => {
+    if (books[key].author == author) {
+        filteredBooks.push(books[key]);
+    }
+  });
+  res.send(filteredBooks)
+  //   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
